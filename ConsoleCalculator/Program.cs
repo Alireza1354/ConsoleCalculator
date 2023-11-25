1bc568bf122254917fcf20db85d7543a19e1a875
Alex <karimfromlfco@gmail.com>
2023-11-25 09:15:02 +0330

    Call Calculate method in program

--- ConsoleCalculator/Program.cs
+++ ConsoleCalculator/Program.cs
@@ -1,10 +1,30 @@
-﻿namespace ConsoleCalculator
+﻿using static System.Console;
+// Additional input validation ommited for brevity
+
+namespace ConsoleCalculator
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            WriteLine("Enter first number");
+            int number1 = int.Parse(ReadLine()!);
+
+            WriteLine("Enter second number");
+            int number2 = int.Parse(ReadLine()!);
+
+            WriteLine("Enter operation");
+            string operation = ReadLine()!.ToUpperInvariant();
+
+            var calculator = new Calculator();
+            var result = calculator.Calculate(number1, number2, operation);
+            DisplayResult(result);
+
+            WriteLine("\nPress enter to exit");
+            ReadLine();
+
+
+            static void DisplayResult(int result) => WriteLine($"Result is: {result}");
         }
     }
 }
